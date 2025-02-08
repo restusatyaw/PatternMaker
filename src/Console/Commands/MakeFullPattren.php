5bf3b70f7610960cd5b71dd177f9fdfe6afdf492
Bangkitaja <bangkitajabang@gmail.com>
2025-02-08 21:39:33 +0800

    fix error

--- src/Console/Commands/MakeFullPattren.php
+++ src/Console/Commands/MakeFullPattren.php
@@ -87,19 +87,32 @@ class MakeFullPattren extends Command
             {
                 return $name::destroy(\$id);
             }
+
+            public function getFilteredData(\$filterData)
+            {
+                \$data = $name::query();
+
+                if (isset(\$filterData['search']) && \$filterData['search'] != '') {
+                    \$data->where('name', 'like', '%' . \$filterData['search'] . '%');
+                }
+
+                return \$data;
+            }
         }
         PHP;
     }
 
     private function getServiceTemplate($name)
     {
-        $name = Str::studly($name); 
+        $name = Str::studly($name);
         return <<<PHP
         <?php
 
         namespace App\Services;
 
         use App\Repositories\\$name.''.Repository;
+        use App\DTOs\\$name.''.DTO;
+        use Yajra\DataTables\Facades\DataTables;
 
         class {$name}Service
         {
@@ -112,7 +125,12 @@ class MakeFullPattren extends Command
 
             public function getAll()
             {
-                return \$this->{$name}Repository->getAll();
+                \$data = \$this->{$name}Repository->getAll();
+
+                // Convert each model to DTO
+                return \$data->map(function (\$item) {
+                    return new {$name}DTO(\$item);
+                });
             }
 
             public function findById(\$id)
@@ -122,17 +140,41 @@ class MakeFullPattren extends Command
 
             public function create(array \$data)
             {
-                return \$this->{$name}Repository->create(\$data);
+                // Convert incoming data to DTO
+                \$dto = new {$name}DTO((object) \$data);
+
+                // Create new entity using the repository
+                return \$this->{$name}Repository->create(\$dto);
             }
 
             public function update(\$id, array \$data)
             {
-                return \$this->{$name}Repository->update(\$id, \$data);
+                // Convert incoming data to DTO
+                \$dto = new {$name}DTO((object) \$data);
+
+                // Update entity using the repository
+                return \$this->{$name}Repository->update(\$id, \$dto);
             }
 
             public function delete(\$id)
             {
                 return \$this->{$name}Repository->delete(\$id);
+            }
+
+            public function getDatatable(\$filterData)
+            {
+                \$data = \$this->{$name}Repository->getFilteredData(\$filterData);
+
+                return DataTables::of(\$data)
+                    ->addColumn('action', function (\$data) {
+                        \$action = '';
+                        // Custom action logic can go here
+                        return \$action;
+                    })
+                    ->editColumn('object_name', function (\$data) {
+                        return \$data->object_name ?? 'N/A';  // Add a fallback for null values
+                    })
+                    ->rawColumns(['action'])->make(true);
             }
         }
         PHP;
@@ -151,14 +193,6 @@ class MakeFullPattren extends Command
                 public int \$id,
                 public string \$name
             ) {}
-
-            public static function fromModel(\$model): self
-            {
-                return new self(
-                    id: \$model->id,
-                    name: \$model->name
-                );
-            }
         }
         PHP;
     }
